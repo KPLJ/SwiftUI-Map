1314cef213920bd228902885fc73ddb931275870
Ji Wang <44724621+KPLJ@users.noreply.github.com>
2021-09-11 16:31:36 -0400

    Initial commit

---
+++ SwiftUI-Map/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SwiftUI-Map
+//
+//  Created by Ji Wang on 9/11/21.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var viewModel = ContentViewModel()
+    
+    var body: some View {
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .accentColor(Color(.systemPink))
+            .onAppear() {
+                viewModel.checkIfLocationServicesIsEnabled()
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Map/SwiftUI_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapApp.swift
+//  SwiftUI-Map
+//
+//  Created by Ji Wang on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
